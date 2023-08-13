23d6a5e8b7d14c4dca4c031fd7446dae77043efb
goya <choieb9519@gmail.com>
2023-08-14 08:47:21 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        sb.append(str).append("??!");
+        System.out.println(sb.toString());
+    }
+}
