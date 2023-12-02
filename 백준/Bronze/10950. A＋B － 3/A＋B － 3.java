cc46a9a1b98322af0ee8896d26812f75057de12d
goya <choieb9519@gmail.com>
2023-12-02 19:28:46 +0900

    [Bronze V] Title: A+B - 3, Time: 140 ms, Memory: 13160 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T, a, b;
+        T = sc.nextInt();
+        
+        for(int i = 0; i < T; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a+b);            
+        }
+    }
+}
